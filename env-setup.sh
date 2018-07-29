74f97475218662ce38a640e17fa7f64413341d18
Pitchaiah M <pitchaiah.m@timesys.com>
2018-07-29 19:44:11 +0530

    added-arg-to-setup-build

--- env-setup.sh
+++ env-setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
      build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
      xz-utils debianutils iputils-ping libsdl1.2-dev xterm
 
-source oe-init-build-env build-test-himinds
+source oe-init-build-env $1
 
 ../script.sh
 
